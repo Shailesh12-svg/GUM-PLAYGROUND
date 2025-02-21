af60f69d5df5e4fefddbf0fd19507dd60919c0ee
Shailesh <mallicksailesh957@gmail.com>
2025-02-21 13:45:28 +0530

    getting all the devices :Input-Output

---
+++ inputOutput.js
@@ -0,0 +1,16 @@
+
+
+const getDevices =async()=>{
+    try{
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    console.log(devices)
+    }catch(err){
+        console.log(err)
+    }
+
+}
+
+
+
+
+getDevices();
--- scripts.js
+++ scripts.js
@@ -62,3 +62,8 @@ document.querySelector('#stop-record').addEventListener('click',(e)=>{stopRecord
 document.querySelector('#play-record').addEventListener('click',(e)=>{playRecording(e)})
 
 document.querySelector('#share-screen').addEventListener('click',(e)=>{shareScreen(e)})
+
+
+document.querySelector('#audio-input').addEventListener('change',(e)=>{changeAudioInput(e)})
+document.querySelector('#audio-output').addEventListener('change',(e)=>{changeAudioOutput(e)})
+document.querySelector('#video-input').addEventListener('change',(e)=>{changeVideoInput(e)})
